ea5316b66ad03de14128ecf462965456969f0fbe
MiguelRosales1264 <miguelrosales1264@gmail.com>
2025-06-09 16:22:04 -0600

    Updates

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const addNewNote = document.getElementById('addNewNote');
 const notesContainer = document.getElementById('notesContainter');
 const popupContent = document.getElementById('popupContent');
 
+// Create empty array to store the notes in memory
+let notes = JSON.parse(localStorage.getItem('notes')) || [];
+if (notes.length !== 0 ) { renderNotes(); };
+
 function showPopup(innerHTML) {
     if (popupContent.hasChildNodes()) { return; } // Avoids adding multiple layers of popup
     const popup = document.createElement('div');
@@ -14,6 +18,39 @@ function showPopup(innerHTML) {
 
 function closePopup() {
     popupContent.innerHTML = '';
+}
+
+// Adds new notes and saves edited notes to notes array
+function saveNote({ id = null, title, content }) {
+    // Do not save if title is empty
+    if (!title.trim()) { return; }
+
+    if (id) {
+        notes = notes.map(note =>
+            note.id === id ? { ...note, title, content } : note
+        );
+    } else {
+        notes.push({
+            id: Date.now(),
+            title,
+            content
+        });
+    }
+
+    saveNotes();
+    renderNotes();
+    closePopup();
+}
+
+function handleKeyBinds( id = null, titleInput, contentInput, button ) {
+    // Handle the button click
+    button.addEventListener('click', () => {
+        saveNote();
+    });
+
+    // Allow 'Enter' key to submit note
+    bindEnterToInput()
+    bindEnterToInput()
 }
 
 function addNotesPopup() {
@@ -30,6 +67,10 @@ function addNotesPopup() {
             </div>
         </div>
     `);
+
+    const noteTitleInput = document.getElementById('noteTitle');
+    const noteContentInput = document.getElementById('noteContent');
+    const addNoteBtn = document.getElementById('addNoteBtn');
 
     handleKeyBinds();
 }
