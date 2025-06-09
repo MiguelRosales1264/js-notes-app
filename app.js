e64a7f6c4cb0af10b33fcd9ce3dacf2cdaac692d
MiguelRosales1264 <miguelrosales1264@gmail.com>
2025-06-09 15:43:12 -0600

    Create showPopup

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 // Select DOM Elements
-const addNewNote = document.getElementById('addNewElement')
+const addNewNote = document.getElementById('addNewElement');
+
+function showPopup(innerHTML) {
+    if (popupContent.hasChildNodes()) { return; } // Avoids adding multiple layers of popup
+    const popup = document.createElement('div');
+
+    // popup.className = 'popup';
+    popup.innerHTML = innerHTML;
+    popupContent.appendChild(popup);
+}
 
 function addNotesPopup() {
-    showPopup();
+    showPopup(`
+        <div class='add-popup'>
+            <div class='popup-header'>
+                <h3>Add New Note</h3>
+                <button onclick='closePopup()'>❌</button>
+            </div>
+            <div class='note-form'>
+                <input type='text' id='noteTitle' placeholder='Note title...'/>
+                <textarea id='noteContent' placeholder='Write your note...'></textarea>
+                <button id='addNoteBtn' type='button'>Add Note</button>
+            </div>
+        </div>
+    `);
 
     handleKeyBinds();
 }
